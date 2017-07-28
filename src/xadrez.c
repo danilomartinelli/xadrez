fc3a778ef3c0c0544aae1522fd7e889af6eea713
Stephanie Rocha <orocha.stephanie@gmail.com>
2017-07-28 14:19:03 -0300

    Parte 1 - MENU
    prints

--- src/xadrez.c
+++ src/xadrez.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <locale.h>
+
 
 int main () {
+    setlocale(LC_ALL, "portuguese");
+    printf("###################################\n");
+    printf("\t Jogo de Xadrez\n\n\n\n");
+    printf("\t1-Jogar\n\n \t2-Ajuda\n\n \t3-Sobre o jogo\n\n \t4-Sair\n");
+    printf("###################################\n");
+
 
 return 0;
 }
