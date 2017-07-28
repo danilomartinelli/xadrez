eff5001e42006174e84bcd79df87bd87f451ad3e
Danilo Martinelli <danilo@cubos.io>
2017-07-28 16:48:44 -0300

    apply design patterns

--- src/xadrez.c
+++ src/xadrez.c
@@ -1,65 +1,79 @@
+//Declarando libs:
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 #include <locale.h>
 
-
-int main () {
-
-    int menu;
-    setlocale(LC_ALL,"portuguese");
-    do{
-    printf("###################################\n");
-    printf("\t Jogo de Xadrez\n\n\n");
-    printf("\t1-Jogar\n\n \t2-Ajuda\n\n \t3-Sobre o jogo\n\n \t4-Sair\n\n");
-    printf("###################################\n");
-    scanf("%d", &menu);
+// Função para limpar tela
+void clear_screen()
+{
+#ifdef WIN32
     system("cls");
+#else
+    system("clear");
+#endif
+}
 
-    //menu
+//Função inicial:
+int main()
+{
 
-    switch(menu){
+    //Declaração de variáveis:
+    int menu;
+
+    //Setando UTF-8:
+    setlocale(LC_ALL, "portuguese");
+
+    do
+    {
+        printf("###################################\n");
+        printf("\t Jogo de Xadrez\n\n\n");
+        printf("\t1-Jogar\n\n \t2-Ajuda\n\n \t3-Sobre o jogo\n\n \t4-Sair\n\n");
+        printf("###################################\n");
+        scanf("%d", &menu);
+        clear_screen();
+
+        //Menu:
+        switch (menu)
+        {
         case 1:
 
             printf("jogue");
             break;
 
-
         case 2:
-             system("cls");
-             printf("Player 1 são as peças brancas, Player 2 as peças Pretas");
-             printf("O jogo segue todas as regras oficiais. Exceto que:\n");
-             printf("\n      Não é possível fazer movimentos especiais como o roque.\n");
-             printf("Para jogar é necessário digitar a letra correspondente a peça que deseja movimentar.\n");
-             printf("\nLogo depois digitar a linha e a coluna para qual você deseja se movimentar.\n\n");
-             printf("Pressione ENTER para voltar.");
-             getchar();
-             break;
+            clear_screen();
+            printf("Player 1 são as peças brancas, Player 2 as peças Pretas");
+            printf("O jogo segue todas as regras oficiais. Exceto que:\n");
+            printf("\n      Não é possível fazer movimentos especiais como o roque.\n");
+            printf("Para jogar é necessário digitar a letra correspondente a peça que deseja movimentar.\n");
+            printf("\nLogo depois digitar a linha e a coluna para qual você deseja se movimentar.\n\n");
+            printf("Pressione ENTER para voltar.");
+            getchar();
+            break;
 
         case 3:
-             system("cls");
-             printf("\t\t\t########## SOBRE ##########\n");
-             printf("Jogo de xadrez desenvolvido para a disciplina de introdução a lógica de programação");
-             printf("\t\t\t########## EQUIPE ########\n");
-             printf("Abraão Lima Xavier Evangelista\n");
-             printf("Bruno Corrêa Arvan\n");
-             printf("Danilo Leone Di Paolo Martinelli Silva\n");
-             printf("Stephanie Oliveira da Rocha\n\n\n");
-             printf("Pressione ENTER para voltar.");
-             getchar();
-              break;
-
+            clear_screen();
+            printf("\t\t\t########## SOBRE ##########\n");
+            printf("Jogo de xadrez desenvolvido para a disciplina de introdução a lógica de programação");
+            printf("\t\t\t########## EQUIPE ########\n");
+            printf("Abraão Lima Xavier Evangelista\n");
+            printf("Bruno Corrêa Arvan\n");
+            printf("Danilo Leone Di Paolo Martinelli Silva\n");
+            printf("Stephanie Oliveira da Rocha\n\n\n");
+            printf("Pressione ENTER para voltar.");
+            getchar();
+            break;
 
         case 4:
-             exit(0);
-             break;
+            exit(EXIT_SUCCESS);
+            break;
 
         default:
-             printf("Opção inválida");
-       }
-    }while(menu!=4);
+            printf("Opção inválida");
+        }
+    } while (menu != 4);
 
-
-return 0;
+    return EXIT_SUCCESS;
 }
