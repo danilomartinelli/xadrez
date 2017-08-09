5bc26721e5f3ebf5d7ab6523e4365a7a20c82266
Stephanie Rocha <orocha.stephanie@gmail.com>
2017-08-09 18:19:45 -0300

    Função nome_jogadores
    FUNCIONANDO TOPPER

--- src/c.c
+++ src/c.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <string.h>
 
-void nomeDosJogadores (char nomeJogador1, char nomeJogador2) {
-    char nomeJogador1, nomeJogador2;
-    printf("Digite o nome do jogador 1");
-    scanf("%s", &nomeJogador1);
-    printf("Digite o nome do jogador2");
-    scanf("%s", &nomeJogador2);
+void nome_jogadores (char *nomeJogador1, char *nomeJogador2);
+
+int main () {
+    char nomeJogador1[20], nomeJogador2[20];
+    nome_jogadores(nomeJogador1, nomeJogador2);
+    printf("%s, %s", nomeJogador1, nomeJogador2);
+
+    return 0;
 }
+void nome_jogadores (char nomeJogador1[20], char nomeJogador2[20]) {
+    printf("Digite o nome do jogador 1\n");
+    gets(nomeJogador1);
+    printf("Digite o nome do jogador2\n");
+    gets(nomeJogador2);
+    }
