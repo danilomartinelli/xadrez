753735a67bb4e015a485a1eb5ff0b32d55aa54a0
Arvan <arvanbruno@hotmail.com>
2017-08-12 19:57:06 -0300

    CAVALO ATUALIZADAO
    ...

--- src/b.c
+++ src/b.c
@@ -68,16 +68,16 @@ void movimento_torre (char tauleiro[10][10], int linha, int coluna, int linhaNov
     }
 
 void movimento_cavalo (char tabuleiro[10][10], int linhavelha, int colunavelha, int linha, int coluna){
-    int movimento_valido;
+    int *movimento_valido;
     do {
         if(((linhavelha-linha==2)&&(colunavelha-coluna==1)) || ((linhavelha-linha==1)&&(colunavelha-coluna==2)) || ((linha-linhavelha==1)&&(colunavelha-coluna==2)) || ((linha-linhavelha==2)&&(colunavelha-coluna==1)) || ((linhavelha-linha==2)&&(coluna-colunavelha==1)) || ((linhavelha-linha==1)&&(coluna-colunavelha==2)) || ((linha-linhavelha==1)&&(coluna-colunavelha==2)) || ((linha-linhavelha==2)&&(coluna-colunavelha==1))){
             printf("Movimento valido");
-            movimento_valido=1;
+            *movimento_valido=1;
         }
         else {
             printf("Movimento invalido");
-            movimento_valido=0;
+            *movimento_valido=0;
         }
     }
-    while (movimento_valido==0);
+    while (*movimento_valido==0);
 }
